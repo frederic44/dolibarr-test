18ad8bf692780613dc32aaf4cbbcfebca9bb68c4
SaydaTounsi <saydainsat@gmail.com>
2020-01-23 13:56:46 +0100

    ajout sayda

---
+++ src/test/java/ProspectInvalide.java
@@ -0,0 +1,95 @@
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ProspectInvalide {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        String s = System.setProperty("webdriver.gecko.driver", "C:\\firefoxDriver\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void testProspectNomInvalide() throws Exception {
+        driver.get("http://10.131.1.32/dolibarr-10.0.6/dolibarr-10.0.6/htdocs/index.php");
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("test");
+        driver.findElement(By.id("username")).click();
+        driver.findElement(By.id("login_line1")).click();
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("test");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.xpath("//*[@id=\"login_line2\"]/input")).click();
+        driver.findElement(By.xpath("//li[@id='mainmenutd_companies']/div/a/div")).click();
+        driver.findElement(By.linkText("Nouveau tiers")).click();
+        driver.findElement(By.id("customerprospect")).click();
+        new Select(driver.findElement(By.id("customerprospect"))).selectByVisibleText("Prospect");
+        driver.findElement(By.id("customerprospect")).click();
+        driver.findElement(By.name("create")).click();
+        driver.findElement(By.xpath("//div[@id='topmenu-login-dropdown']/a/span")).click();
+
+        try {
+            assertEquals(driver.findElement(By.xpath("//*[@id=\"mainbody\"]/div[8]/div/div[2]/div")).getText(),"Le champ 'Nom du tiers' est obligatoire");
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        driver.findElement(By.linkText("Déconnexion")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+       //driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
